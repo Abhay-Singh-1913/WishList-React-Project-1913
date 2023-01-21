262cb95ce8d6ac5ebe8b719e6fdd83ed2c10fd0b
Abhay <as1240143@gmail.com>
2023-01-21 14:45:22 +0530

    Footer File

---
+++ src/My Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='foot'>
+    <h1>It's a ongoing Project</h1>
+    <span>After complition you can add new palces of different type</span><br />
+    <span>And can add ot it in your wishlist</span>
+    </div>
+  )
+}
+
