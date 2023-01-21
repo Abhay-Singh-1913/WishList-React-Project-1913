a9bc0878297decb21eb72c717273b84eca51916b
Abhay <as1240143@gmail.com>
2023-01-21 14:43:30 +0530

    App file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './My Components/Header.js';
+import Footer from "./My Components/Footer";
+import Mainpage from "./My Components/Mainpage";
+
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Mainpage/>
+      <Footer/>
+    </>
+  );
+}
+export default App;
