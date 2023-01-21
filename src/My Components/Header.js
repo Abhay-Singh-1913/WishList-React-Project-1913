8e9438455ab50761326975deda6b809c5cd85542
Abhay <as1240143@gmail.com>
2023-01-21 14:45:05 +0530

    Header File

---
+++ src/My Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Headcss.css';
+
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <span className='heading'>Are You Ready For It</span>
+        <pre className='slog'>"WE TARVEL NOT TO ESCAPE LIFE <br /> BUT FOR LIFE NOT TO ESCAPE US"
+        </pre>
+      </header>
+    </>
+  );
+};
+
+export default Header;
